152c958207cfae5505fb0982ec60db5dfdf57dc8
plastic12 <choihogwang@gmail.com>
2018-02-10 10:34:06 +0900

    comment change

--- src/main/Input.java
+++ src/main/Input.java
@@ -1,6 +1,5 @@
 package main;
 
-import javafx.scene.paint.Color;
 
 public class Input 
 {
--- src/main/Main.java
+++ src/main/Main.java
@@ -28,11 +28,6 @@ public class Main extends Application {
 		primaryStage.setScene(scene); // Place the scene in the stage
 		primaryStage.show(); // Display the stage
 	}
-
-	/**
-	 * The main method is only needed for the IDE with limited
-	 * JavaFX support. Not needed for running from the command line.
-	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
